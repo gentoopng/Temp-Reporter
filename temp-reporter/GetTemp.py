499008fa4d56edb4eebc80049bea1b2b90e09598
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-06-22 02:12:19 +0900

    getTemp.py 修正2

--- temp-reporter/GetTemp.py
+++ temp-reporter/GetTemp.py
@@ -4,8 +4,8 @@ import serial
 class GetTemp:
     def __init__(self, port, rate):
         self.ser = serial.Serial()
-        self.port = port
-        self.baudrate = rate
+        self.ser.port = port
+        self.ser.baudrate = rate
         self.ser.setDTR(False)
         self.ser.open()
 
