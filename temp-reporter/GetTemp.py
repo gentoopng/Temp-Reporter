92ca484d5633f7a1669c4e61c1b9b3ce61e1d3c1
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-06-22 01:25:04 +0900

    GetTemp.py 実装

---
+++ temp-reporter/GetTemp.py
@@ -0,0 +1,19 @@
+# ライブラリ pySerialを使用
+import serial
+
+class GetTemp:
+    def __init__(self, port, rate):
+        self.ser = serial.Serial()
+        self.port = port
+        self.baudrate = rate
+        self.ser.setDTR(False)
+        self.ser.open()
+
+
+    def getFromArduino(self, port, rate):
+        line = self.ser.readLine()
+        values = list(float(line.split()))
+        return values
+    
+    def closeSerialPort(self):
+        self.ser.close()
