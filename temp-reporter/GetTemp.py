7e38e8621fde724d8d65106cfbdf93ba5cfb243b
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-06-22 09:47:55 +0900

    シリアルポートの扱いを変更

--- temp-reporter/GetTemp.py
+++ temp-reporter/GetTemp.py
@@ -9,10 +9,10 @@ class GetTemp:
         self.ser.port = port
         self.ser.baudrate = rate
         self.ser.setDTR(False)
-        self.ser.open()
 
 
     def getFromArduino(self):
+        self.ser.open()
         line1, line2 = self.ser.readline().split()
 
         humidity = "".join(re.findall("[0-9][0-9]+\.+[0-9][0-9]", str(line1)))
@@ -20,7 +20,7 @@ class GetTemp:
 
         # print(humidity)
         # print(temp)
-
+        self.closeSerialPort()
         values = {"humidity": float(humidity), "temp": float(temp)}
         return values
 
